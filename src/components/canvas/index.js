ce061e3a90269f1756d1ea38cb0731ecd18171e6
Hassan <hassan1652007@gmail.com>
2025-03-17 21:47:56 +0500

    first commit

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import HackerRoom from "./HackerRoom";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, HackerRoom, StarsCanvas };
